3576b6be12b8cfbc03bc81454f083d13cab38ea5
Quinton LaBorde <quintonlaborde@gmail.com>
2022-01-10 14:31:33 -0700

    added onclick handler

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -8,9 +8,9 @@ const ExpenseDate = (props) => {
 
   return (
     <div className="expense-date">
-      <div className="expende-date__month">{month}</div>
-      <div className="expende-date__year">{year}</div>
-      <div className="expende-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
